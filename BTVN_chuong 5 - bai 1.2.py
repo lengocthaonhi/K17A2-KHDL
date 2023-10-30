1d25eb6216be2ee26551f0e154989489cac5f03d
lengocthaonhi <lntnhi.dhkl17a2hn@sv.uneti.edu.vn>
2023-10-30 12:39:46 +0700

    Thêm file bài tập chương 5 Tin học cơ sở!

---
+++ BTVN_chuong 5 - bai 1.2.py
@@ -0,0 +1,9 @@
+# Bài 1.2: Tính toán đơn giản các phép toán số học
+print("Bài 1.2: Tính toán đơn giản các phép toán số học")
+x=10
+y=5
+print("x=",x,",","y=",y)
+print("Tổng: x+y=",x+y)
+print("Hiệu: x-y=",x-y)
+print("Tích: x*y=",x*y)
+print("Thương: x/y=",x/y)
---
+++ BTVN_chuong 5 - bai 1.3.py
@@ -0,0 +1,9 @@
+# Bài 1.3: Mua hàng (sữa hộp Vinamilk)
+print("Bài 1.3: Mua hàng (sữa hộp Vinamilk)")
+TH="Sữa hộp Vinamilk"
+SL=5
+DG=25000
+print("Tên hàng:",TH)
+print("Số lượng:",SL)
+print("Đơn giá:",DG)
+print("Tiền phải trả:",SL*DG,"vnđ")
